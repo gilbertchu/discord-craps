ce4bc3d2818898a6491b53df805b96d93640b580
Gilbert Chu <gilbert@gilbertchu.com>
2024-02-18 09:10:43 -0800

    test slash command user

--- index.js
+++ index.js
@@ -1,16 +1,65 @@
-import CrapsPlayer from "./libs/CrapsPlayer.js"
-import Craps from "./libs/Craps.js"
+// Require the necessary discord.js classes
+const { Client, Collection, Events, GatewayIntentBits } = await import('discord.js');
+const { default: {clientId, guildId, token} } = await import('./config.json', {assert: {type: "json"}});
+const { REST, Routes } = await import('discord.js');
+import userCommand from './commands/user.js'
 
-const p = new CrapsPlayer('G', 200)
-Craps.addPlayer(p)
 
-import readline from 'node:readline'
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+client.commands.set(userCommand.data.name, userCommand);
+const commands = []
+commands.push(userCommand.data.toJSON())
 
-const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
 
-while (true) {
-  rl.question('', answer => {
-    console.log(`TODO Attempt ${answer}`)
-    rl.close()
-  })
-}
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
+// Log in to Discord with your client's token
+client.login(token);
